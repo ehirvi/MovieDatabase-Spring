1cc2c9575cc47f8659f279d8d5785e9478c4f740
ehirvi <eino.hirvi@gmail.com>
2023-10-22 21:33:08 +0300

    First commit

---
+++ src/test/java/hh/sof03/moviedatabase/MoviedatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.sof03.moviedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviedatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
